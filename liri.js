a7db819cc9e49ccd8e7565010776d4abc711cfac
DHLAP\h <vdavidhammond@gmail.com>
2017-07-12 12:09:58 -0600

    Got tweets showing when you type in my-tweets

--- liri.js
+++ liri.js
@@ -1,7 +1,14 @@
 var keyLink = require("./keys.js");
-console.log(keyLink.twitKeys);
+// console.log(process.argv);
 var Twitter = require('twitter');
-
+// This function determines what you are entering and then performs that task
+function whatToShow (vari) {
+  if (vari === "my-tweets") {
+    myTweets();
+  }
+};
+// This function will show my most recent tweets
+function myTweets () {
 var client = new Twitter(keyLink.twitKeys);
 
 var params = {screen_name: 'nodejs'};
@@ -10,3 +17,5 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     console.log(tweets);
   }
 });
+};
+whatToShow (process.argv[2]);
