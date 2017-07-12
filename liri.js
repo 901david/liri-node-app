95d4eb15ccd6d4e8fc597c63b993215615136af4
DHLAP\h <vdavidhammond@gmail.com>
2017-07-12 12:23:50 -0600

    Now the app can also tweet

--- liri.js
+++ liri.js
@@ -1,21 +1,44 @@
-var keyLink = require("./keys.js");
-// console.log(process.argv);
-var Twitter = require('twitter');
-// This function determines what you are entering and then performs that task
-function whatToShow (vari) {
-  if (vari === "my-tweets") {
-    myTweets();
-  }
-};
-// This function will show my most recent tweets
-function myTweets () {
-var client = new Twitter(keyLink.twitKeys);
+// This section controls Twitter Functionality
+        var keyLink = require("./keys.js");
+        // console.log(process.argv);
+        var Twitter = require('twitter');
+        // This function determines what you are entering and then performs that task
+        function whatToShow (vari) {
+          if (vari === "my-tweets") {
+            myTweets();
+          }
+          else if (vari === "tweet-this") {
+            tweetThis();
+            console.log(process.argv);
+          }
+        };
+        // This function will show my most recent tweets
+        function myTweets () {
+        var client = new Twitter(keyLink.twitKeys);
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-};
-whatToShow (process.argv[2]);
+        var params = {screen_name: 'nodejs'};
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if (!error) {
+            console.log(tweets);
+          }
+        });
+        };
+        whatToShow (process.argv[2]);
+// This section will allow Twitter posts
+      function tweetThis (vari) {
+
+
+        var T = new Twitter(keyLink.twitKeys);
+        var tweet = {
+        status: process.argv[3] }
+        T.post('statuses/update', tweet, tweeted)
+        function tweeted(err, data, response) {
+        if(err){
+        console.log("Something went wrong!");
+        }
+        else{
+        console.log("Voila It worked!");
+        }
+        }
+      };
+  // This section will control Spotify Functionality
