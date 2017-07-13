3c74b07c06bbe76d00a925289afe2e67c240f107
DHLAP\h <vdavidhammond@gmail.com>
2017-07-12 21:27:20 -0600

    Movie search functionality added in.

--- liri.js
+++ liri.js
@@ -14,6 +14,9 @@
           else if (vari === "spotify-this-song") {
             spotifyThis();
           }
+          else if (vari === "movie-this") {
+            movieRequest();
+          }
           else {
             console.log("Not a valid command.  Try again.");
           }
@@ -58,10 +61,40 @@
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    for (var key in data) {
-      console.log("key: " + key + " value: " + data[key]);
-    }
+    var entireObject = data;
+    for (var outside in data) {
+      // console.log(data[outside].items);
+      for (var i = 0; i < (data[outside].items).length; i++) {
+        console.log((data[outside].items)[i]);
+      }
+    };
+
+    // console.log(entireObject);
 
   });
 
+};
+function movieRequest () {
+var request = require("request");
+searchCrit = process.argv[3] || "Mr.+Nobody";
+request('http://www.omdbapi.com/?apikey=40e9cece&t=' + searchCrit, function (error, response, body) {
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  var movieObj = JSON.parse(body);
+  console.log("Title: " + movieObj.Title);
+  console.log("Year: " + movieObj.Year);
+  console.log("Rating: " + movieObj.Rated);
+  console.log("Country Produced In: " + movieObj.Country);
+  console.log("Language: " + movieObj.Language);
+  console.log("Plot: " + movieObj.Plot);
+  console.log("Actors/Actresses: " + movieObj.Actors);
+  console.log("Rotten Tomatoes: " + movieObj.imdbRating);
+
+
+
+
+
+
+});
 };
